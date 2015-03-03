95794ddea5238cd8284707ef70a6278485377760
vserpa <vinicius-serpa@live.com>
2015-03-02 23:49:00 -0300

    Add NHibernate with Repositories, CRUD and Tests

---
+++ RESTful/ContactManager.Test/GenerateSchemaTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NHibernate.Cfg;
+using ContactManager.Models;
+using NHibernate.Tool.hbm2ddl;
+
+namespace ContactManager.Test
+{
+    [TestClass]
+    public class GenerateSchemaTest
+    {
+        [TestMethod]
+        public void CanGenerateSchema()
+        {
+            var cfg = new Configuration();
+            cfg.Configure();
+            cfg.AddAssembly(typeof(Product).Assembly);
+
+            new SchemaExport(cfg).Execute(false, true, false);
+        }
+    }
+}
---
+++ RESTful/ContactManager/Repositories/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using ContactManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContactManager.Repositories
+{
+    public interface IProductRepository
+    {
+        void Add(Product product);
+        void Update(Product product);
+        void Remove(Product product);
+        Product GetById(Guid productId);
+        Product GetByName(string name);
+        ICollection<Product> GetByCategory(string category);
+    }
+}

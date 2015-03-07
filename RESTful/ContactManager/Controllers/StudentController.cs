64719bf53277ac5eddd060fca50ebac499b99799
vserpa <vinicius-serpa@live.com>
2015-03-07 16:05:06 -0300

    EF6 with Code First and DB Generate

---
+++ RESTful/ContactManager/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using ContactManager.Repositories.RepositoriesImplEF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContactManager.Controllers
+{
+    public class StudentController : Controller
+    {
+
+        private SchoolContext db = new SchoolContext();
+        
+        //
+        // GET: /Student/
+        public ViewResult Index()
+        {
+            return View(db.Students.ToList());
+        }
+
+    }
+}
---
+++ RESTful/ContactManager/Repositories/RepositoriesImplEF/SchoolContext.cs
@@ -0,0 +1,30 @@
+﻿using ContactManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace ContactManager.Repositories.RepositoriesImplEF
+{
+    // The Context needs declare on WebConfig, EF section
+    public class SchoolContext : DbContext
+    {
+        // SchoolContext is the name of ConnectionString in Web.config nas is the defaul class name (can exclude from base command)
+        public SchoolContext() : base("SchoolContext")
+        {
+        }
+        
+        public DbSet<Student> Students { get; set; }
+        
+        // Enrollments and Courses can excluded from here because Students have relationships with him and EF makes auto
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}

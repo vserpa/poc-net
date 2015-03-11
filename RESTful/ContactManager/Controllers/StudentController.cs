d94105751510b083ca52c63db53cef44275d5af4
vserpa <vinicius-serpa@live.com>
2015-03-11 00:38:38 -0300

    CRUD with EF (create amd read)

--- RESTful/ContactManager/Controllers/StudentController.cs
+++ RESTful/ContactManager/Controllers/StudentController.cs
@@ -1,7 +1,10 @@
-﻿using ContactManager.Repositories.RepositoriesImplEF;
+﻿using ContactManager.Models;
+using ContactManager.Repositories.RepositoriesImplEF;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -17,6 +20,48 @@ namespace ContactManager.Controllers
         public ViewResult Index()
         {
             return View(db.Students.ToList());
+        }
+
+        public ActionResult Details (int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        // AntiForgeryToken prevent cross-site request attacks and the view needs call Html.AntiForgeryToken() (security issue)
+        [ValidateAntiForgeryToken]
+        // Bind cmd Remove id from model bind and determine exactly what you need update (is a security issue too)
+        public ActionResult Create([Bind(Include = "LastName, FirstMidName, EnrollmentDate")]Student student)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Students.Add(student);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (DataException /* dex */)
+            {
+                //Log the error (uncomment dex variable name and add a line here to write a log.
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+            }
+            return View(student);
         }
 
     }
